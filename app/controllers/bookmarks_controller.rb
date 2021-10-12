abd0f3e02e34e10989e3a0d7b238be81bc8137da
Dennis <dennisj86@protonmail.com>
2021-10-12 19:37:04 +0200

    solution

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,40 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show edit update destroy]
+
+  def index
+    @list = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
